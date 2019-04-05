52b97a89ba576a1962524c7e7989d0d66ef0d1de
Ben Awad <benawad97@gmail.com>
2019-04-04 20:37:08 -0500

    formik field array

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField } from "@material-ui/core";
+import { Field, FieldArray, FieldProps, Form, Formik, getIn } from "formik";
+import * as React from "react";
+import { generate } from "shortid";
+import * as yup from "yup";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const validationSchema = yup.object().shape({
+  people: yup.array().of(
+    yup.object().shape({
+      firstName: yup.string().max(10),
+      lastName: yup.string().min(2)
+    })
+  )
+});
+
+const Input = ({ field, form: { errors } }: FieldProps) => {
+  const errorMessage = getIn(errors, field.name);
+
+  return (
+    <>
+      <TextField {...field} />
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Formik
+        initialValues={{
+          people: [{ id: "5", firstName: "bob", lastName: "bob2" }]
+        }}
+        onSubmit={() => {}}
+        validationSchema={validationSchema}
+      >
+        {({ values, errors }) => (
+          <Form>
+            <FieldArray name="people">
+              {({ push, remove }) => (
+                <div>
+                  {values.people.map((p, index) => {
+                    return (
+                      <div key={p.id}>
+                        <Field
+                          name={`people[${index}].firstName`}
+                          component={Input}
+                        />
+                        <Field
+                          name={`people[${index}].lastName`}
+                          component={Input}
+                        />
+                        <div onClick={() => remove(index)}>x</div>
+                      </div>
+                    );
+                  })}
+                  <Button
+                    type="button"
+                    onClick={() =>
+                      push({ id: generate(), firstName: "", lastName: "" })
+                    }
+                  >
+                    add to list
+                  </Button>
+                </div>
+              )}
+            </FieldArray>
+            <div>
+              <Button type="submit">submit</Button>
+            </div>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+            <pre>{JSON.stringify(errors, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default App;
